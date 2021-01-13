1ab875e6fb94459f64a6f91894b349e243852305
Suyash Patil <suyashpatil532001@gmail.com>
2021-01-13 13:11:34 +0530

    implemented game logic

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!!';
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!!';
+    } else if(guess > secretNumber) {
+        if(score>1) {
+            document.querySelector('.message').textContent = '⬆⬆ Too High!!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You lost the game!!";
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    } else {
+        if(score>1){    
+            document.querySelector('.message').textContent = '⬇⬇ Too Low!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = "You lost the game!!";
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
